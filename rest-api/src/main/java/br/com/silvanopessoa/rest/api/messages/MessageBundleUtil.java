101c8ec6efcd0ca242c346d7b720cdba3fe3b298
Silvano <silvano.pessoa@gmail.com>
2015-05-20 10:42:49 -0300

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

--- rest-api/src/main/java/br/com/silvanopessoa/rest/api/messages/MessageBundleUtil.java
+++ rest-api/src/main/java/br/com/silvanopessoa/rest/api/messages/MessageBundleUtil.java
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * Produto: REST                                                              *
+ *                                                                            *
+ *    History:                                                                *
+ *          Data        Programador              Tarefa                       *
+ *          ----------  -----------------        -----------------------------*
+ *   Autor  20/05/2015  silvano.pessoa          Classe criada.                *
+ *                                                                            *
+ *   Comments:                                                                *
+ *                                                                            *
+ *                                                                            *
+ *****************************************************************************/
 package br.com.silvanopessoa.rest.api.messages;
 
 import java.util.Locale;
@@ -6,9 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
+/**
+ * The Class MessageBundleUtil.
+ */
 @Component
 public class MessageBundleUtil {
 
+    /** O(a)(s) msg. */
     @Autowired
     private ResourceBundleMessageSource msg;
     
@@ -16,10 +32,23 @@ public class MessageBundleUtil {
     public static final String PT_BR = "pt_BR";
 
     
+	/**
+	 * Obtém o valor do(a)(s) message.
+	 *
+	 * @param code O(a)(s) code
+	 * @param args O(a)(s) args
+	 * @return O(a)(s) message
+	 */
 	public final String getMessage(String code, Object[] args){
 		return msg.getMessage(code,args,new Locale(PT_BR));
 	}
 	
+	/**
+	 * Obtém o valor do(a)(s) message.
+	 *
+	 * @param code O(a)(s) code
+	 * @return O(a)(s) message
+	 */
 	public final String getMessage(String code){
 		return msg.getMessage(code,new Object[] {},new Locale(PT_BR));
 	}
