b7130f780ca084ec02c74885f858d76e8d496b51
Silvano <silvano.pessoa@gmail.com>
2015-07-14 13:24:38 -0300

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

---
+++ rest-lang/src/main/java/br/com/silvanopessoa/rest/lang/exception/ResourceUnsupportedMediaTypeException.java
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * Produto: REST                                                              *
+ *                                                                            *
+ *    History:                                                                *
+ *          Data        Programador              Tarefa                       *
+ *          ----------  -----------------        -----------------------------*
+ *   Autor  13/07/2015  silvano.pessoa          Classe criada.                *
+ *                                                                            *
+ *   Comments:                                                                *
+ *                                                                            *
+ *                                                                            *
+ *****************************************************************************/
+package br.com.silvanopessoa.rest.lang.exception;
+
+import cz.jirutka.spring.exhandler.messages.ErrorMessage;
+
+/**
+ * The Class ResourceUnsupportedMediaTypeException.
+ */
+public class ResourceUnsupportedMediaTypeException extends RuntimeException{
+
+	/** A Constante serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+	
+	/** O(a)(s) error message. */
+	private ErrorMessage errorMessage;
+
+	/**
+	 * Instancia um novo(a) resource unsupported media type exception.
+	 */
+	public ResourceUnsupportedMediaTypeException() {
+		super();
+	}
+
+	/**
+	 * Instancia um novo(a) resource unsupported media type exception.
+	 *
+	 * @param message O(a)(s) message
+	 * @param cause O(a)(s) cause
+	 * @param enableSuppression O(a)(s) enable suppression
+	 * @param writableStackTrace O(a)(s) writable stack trace
+	 */
+	public ResourceUnsupportedMediaTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	/**
+	 * Instancia um novo(a) resource unsupported media type exception.
+	 *
+	 * @param message O(a)(s) message
+	 * @param cause O(a)(s) cause
+	 */
+	public ResourceUnsupportedMediaTypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Instancia um novo(a) resource unsupported media type exception.
+	 *
+	 * @param message O(a)(s) message
+	 */
+	public ResourceUnsupportedMediaTypeException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Instancia um novo(a) resource unsupported media type exception.
+	 *
+	 * @param cause O(a)(s) cause
+	 */
+	public ResourceUnsupportedMediaTypeException(Throwable cause) {
+		super(cause);
+	}
+	
+	/**
+	 * Obtém o valor do(a)(s) error message.
+	 *
+	 * @return O(a)(s) error message
+	 */
+	public ErrorMessage getErrorMessage() {
+		return errorMessage;
+	}
+}
---
+++ rest-lang/src/main/java/br/com/silvanopessoa/rest/lang/exception/handler/ResourceInvalidMimeTypeHandler.java
@@ -0,0 +1,111 @@
+package br.com.silvanopessoa.rest.lang.exception.handler;
+
+import static br.com.silvanopessoa.rest.lang.type.RFC2616Type.UNSUPPORTED_MEDIA_TYPE_SPEC;
+import static br.com.silvanopessoa.rest.lang.type.RFC2616Type.UNSUPPORTED_MEDIA_TYPE_URI;
+import static org.springframework.http.HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.InvalidMimeTypeException;
+
+import cz.jirutka.spring.exhandler.handlers.ErrorMessageRestExceptionHandler;
+import cz.jirutka.spring.exhandler.messages.ErrorMessage;
+import cz.jirutka.spring.exhandler.messages.ValidationErrorMessage;
+
+public class ResourceInvalidMimeTypeHandler extends ErrorMessageRestExceptionHandler<InvalidMimeTypeException>{
+
+	/** LOG. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceInvalidMimeTypeHandler.class);
+
+    /** O(a)(s) error message. */
+    private ErrorMessage errorMessage;
+    
+	public ResourceInvalidMimeTypeHandler() {
+		super(UNSUPPORTED_MEDIA_TYPE);
+	}
+
+	/* (non-Javadoc)
+	 * @see cz.jirutka.spring.exhandler.handlers.ErrorMessageRestExceptionHandler#createBody(java.lang.Exception, javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+    public ErrorMessage createBody(InvalidMimeTypeException ex, HttpServletRequest req) {
+        ErrorMessage tmpl = super.createBody(ex, req);
+        ValidationErrorMessage msg = new ValidationErrorMessage(tmpl);
+        if(errorMessage==null){
+        	errorMessage = new ErrorMessage();
+        }
+       	msg.setType(this.getType());
+       	msg.setTitle(this.getTitle());
+       	msg.setDetail(this.getDetail());
+       	msg.setInstance(this.getUriInstance());
+        return msg;
+    }
+	
+	/**
+	 * Obtém o valor do(a)(s) detail.
+	 *
+	 * @return O(a)(s) detail
+	 */
+	private String getDetail(){
+        if(errorMessage.getDetail()==null){
+        	return HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase();
+        }else{
+        	return errorMessage.getDetail();
+        }
+	}
+	
+	/**
+	 * Obtém o valor do(a)(s) title.
+	 *
+	 * @return O(a)(s) title
+	 */
+	private String getTitle(){
+        if(errorMessage.getTitle()==null){
+        	return HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase();
+        }else{
+        	return errorMessage.getTitle();
+        }
+	}
+	
+	/**
+	 * Obtém o valor do(a)(s) type.
+	 *
+	 * @return O(a)(s) type
+	 */
+	private URI getType(){
+		 if(errorMessage.getType()==null){
+			try {
+				return new URI(UNSUPPORTED_MEDIA_TYPE_SPEC);
+			} catch (URISyntaxException e) {
+				LOGGER.error("Erro ao criar URI " + UNSUPPORTED_MEDIA_TYPE_SPEC, e);
+			}
+		 }else{
+			 return errorMessage.getType();
+		 }
+		return null;
+	}
+
+	/**
+	 * Obtém o valor do(a)(s) uri instance.
+	 *
+	 * @return O(a)(s) uri instance
+	 */
+	private URI getUriInstance(){
+		 if(errorMessage.getInstance()==null){
+			try {
+				return new URI(UNSUPPORTED_MEDIA_TYPE_URI);
+			} catch (URISyntaxException e) {
+				LOGGER.error("Erro ao criar URI " + UNSUPPORTED_MEDIA_TYPE_URI, e);
+			}
+		 }else{
+			 return errorMessage.getInstance();
+		 }
+		return null;
+	}
+}
