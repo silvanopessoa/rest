73443085547422bee224d1c422f60b4da0d174c4
Silvano <silvano.pessoa@gmail.com>
2015-07-16 17:19:29 -0300

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

--- rest-oauth2/src/main/java/br/com/silvanopessoa/rest/oauth2/provider/config/UserDetailsServiceImpl.java
+++ rest-oauth2/src/main/java/br/com/silvanopessoa/rest/oauth2/provider/config/UserDetailsServiceImpl.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-import org.springframework.util.InvalidMimeTypeException;
 
 import br.com.silvanopessoa.rest.model.Usuario;
 
