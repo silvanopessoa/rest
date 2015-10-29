d46a42e57c6e5b731c788ce77bdf44a980f3a710
Silvano <silvano.pessoa@gmail.com>
2015-10-29 17:27:35 -0200

    Adicionado Swagger

---
+++ rest-api/src/main/java/br/com/silvanopessoa/rest/api/conf/swagger/ApplicationSwaggerConfig.java
@@ -0,0 +1,9 @@
+package br.com.silvanopessoa.rest.api.conf.swagger;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+public class ApplicationSwaggerConfig {
+
+
+}
