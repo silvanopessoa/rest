723dcfee5daa7c675f48b2d249a410c814ed5ecd
Silvano <silvano.pessoa@gmail.com>
2015-11-16 17:16:31 -0200

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

--- rest-api/src/main/java/br/com/silvanopessoa/rest/api/controller/UsuarioController.java
+++ rest-api/src/main/java/br/com/silvanopessoa/rest/api/controller/UsuarioController.java
@@ -14,8 +14,12 @@ package br.com.silvanopessoa.rest.api.controller;
 
 import static br.com.silvanopessoa.rest.api.controller.doc.Doc.READ_DESCRIPTION;
 import static br.com.silvanopessoa.rest.api.controller.doc.Doc.READ_SCOPE;
+import static br.com.silvanopessoa.rest.api.controller.doc.Doc.USUARIO_NOTES_DELETE_BY_LOGIN;
 import static br.com.silvanopessoa.rest.api.controller.doc.Doc.USUARIO_NOTES_GET_BY_LOGIN;
+import static br.com.silvanopessoa.rest.api.controller.doc.Doc.USUARIO_VALUE_DELETE_BY_LOGIN;
 import static br.com.silvanopessoa.rest.api.controller.doc.Doc.USUARIO_VALUE_GET_BY_LOGIN;
+import static br.com.silvanopessoa.rest.api.controller.doc.Doc.WRITE_DESCRIPTION;
+import static br.com.silvanopessoa.rest.api.controller.doc.Doc.WRITE_SCOPE;
 import static br.com.silvanopessoa.rest.api.helper.IdentidadeHelper.getClienteIdFromAuth;
 import static br.com.silvanopessoa.rest.lang.base.PreconditionsRest.checkNotFound;
 import static org.apache.http.HttpHeaders.IF_MODIFIED_SINCE;
@@ -28,20 +32,19 @@ import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
-
-import javax.annotation.security.RolesAllowed;
-
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import io.swagger.annotations.AuthorizationScope;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -51,6 +54,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.silvanopessoa.model.usuario.v1.UsuarioType;
 import br.com.silvanopessoa.rest.api.conf.swagger.ApplicationSwaggerConfig;
+import br.com.silvanopessoa.rest.api.controller.doc.Doc;
 import br.com.silvanopessoa.rest.api.validation.UsuarioValidator;
 import br.com.silvanopessoa.rest.model.Usuario;
 import br.com.silvanopessoa.rest.model.transformation.UsuarioResourceAssembler;
@@ -148,7 +152,7 @@ public class UsuarioController {
             LOGGER.info("PUT USUARIO | O usuário será atualizado. Usuário: "+login+" Entity:" + usuario);
             Usuario usuarioEntity = assembler.toEntity(usuario);
             Usuario usuarioAlterado = service.updateUsuario(login, usuarioEntity, clienteId);
-            responseEntity = new ResponseEntity<Void>(header.updateHeaders(usuarioAlterado), NO_CONTENT);//TODO: UPDATE TEM LOCATION?
+            responseEntity = new ResponseEntity<Void>(header.updateHeaders(usuarioAlterado), NO_CONTENT);
         }
         return responseEntity;
     }
@@ -157,22 +161,34 @@ public class UsuarioController {
     /**
      * Exclui o(a) usuario.
      * 
-     * Resposta: 204 (No Content) if the action has been enacted but the response does not include an entity.
-     * Resposta: 404 (Not found)
+     * @param login o(a) login
      * 
      * @author silvano.pessoa
      * @see http://restpatterns.org/HTTP_Methods/DELETE
-     * @param login o(a) login
      */
-    @ResponseStatus(NO_CONTENT)
+	@ApiOperation(
+    		value = USUARIO_VALUE_DELETE_BY_LOGIN, 
+    		notes = USUARIO_NOTES_DELETE_BY_LOGIN, 
+    		authorizations = {@Authorization(
+    			value = ApplicationSwaggerConfig.securityOAuth2, 
+				scopes = {@AuthorizationScope( scope = WRITE_SCOPE, description = WRITE_DESCRIPTION)})})
+	@ApiResponses(value = { 
+		@ApiResponse(code = 200, message = "OK"),
+		@ApiResponse(code = 202, message = "Accepted"), 
+		@ApiResponse(code = 204, message = "No Content"), 
+		@ApiResponse(code = 401, message = "Unauthorized"), 
+		@ApiResponse(code = 403, message = "Forbidden"),		
+		@ApiResponse(code = 404, message = "Not Found")})
+	@RolesAllowed({"ROLE_REST_USER_DELETE"})
     @RequestMapping(value = "/{login}", method = DELETE, produces = { APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE })
-    public void deleteUsuario(@PathVariable("login") String login) {
-        LOGGER.info("DELETE USUARIO | Iniciado | Usuário:" + login);
+    public ResponseEntity<Void> deleteUsuario(@PathVariable("login") String login) {
+        LOGGER.info("DELETE USUARIO | Iniciado | Inativa o usuário:" + login);
         String clienteId = getClienteIdFromAuth();
         validator.checkDeleteRequest(login, clienteId);
         checkNotFound(service.findByLoginAndClienteId(login, clienteId),msg.deleteCheckNotFoundMessage(login));
         service.deleteUsuario(login, clienteId);
         LOGGER.info("DELETE USUARIO | Concluído | Usuário:" + login);
+		return new ResponseEntity<>(NO_CONTENT);
     }
     
     /**
@@ -182,6 +198,9 @@ public class UsuarioController {
      * @param login o(a) login
      * @param dataAlteracao o(a) data alteracao
      * @return o(a) usuario
+     * 
+     * @author silvano.pessoa
+     * @see http://www.restpatterns.org/HTTP_Methods/GET
      */
 	@ApiOperation(
     		value = USUARIO_VALUE_GET_BY_LOGIN, 
@@ -190,21 +209,22 @@ public class UsuarioController {
     			value = ApplicationSwaggerConfig.securityOAuth2, 
 				scopes = {@AuthorizationScope( scope = READ_SCOPE, description = READ_DESCRIPTION)})})
 	@ApiResponses(value = { 
-		@ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 304, message = "Not Modified"),
-		@ApiResponse(code = 401, message = "Unauthorized"), @ApiResponse(code = 403, message = "Forbidden"),		
+		@ApiResponse(code = 200, message = "OK"), 
+		@ApiResponse(code = 304, message = "Not Modified"),
+		@ApiResponse(code = 401, message = "Unauthorized"), 
+		@ApiResponse(code = 403, message = "Forbidden"),		
 		@ApiResponse(code = 404, message = "Not Found")})
 	@RolesAllowed({"ROLE_REST_USER_FIND"})
-//	@PreAuthorize("hasRole('ROLE_REST_USER_FIND')")
 	@RequestMapping(value = "/{login}",method = GET, produces = { APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE })
     public ResponseEntity<UsuarioType> getUsuario(@PathVariable("login") String login, @RequestHeader(value = IF_MODIFIED_SINCE, required = false) String dataAlteracao) {
-		LOGGER.debug("GET USUARIO | Iniciado | Obtem o usuário.");
+		LOGGER.debug("GET USUARIO | Iniciado | Obtem o usuário. {}");
 		String clienteId = getClienteIdFromAuth();
-        LOGGER.debug("GET USUARIO | Iniciado | Obtem o usuário. Identicador do usuário: {}, data alteracao: {}, cliente: {}",login,dataAlteracao,clienteId);
+        LOGGER.debug("GET USUARIO | Identificador | Obtem o usuário. Identicador do usuário: {}, cliente: {}, data alteracao: {}",login,clienteId,dataAlteracao);
         validator.checkGetRequest(login, dataAlteracao, clienteId);
         Usuario usuario = service.getUsuario(login, dataAlteracao, clienteId);
         validator.checkGetResponse(usuario);
         UsuarioType resource = assembler.toResource(usuario);
-        LOGGER.debug("GET USUARIO | Concluído | Obtem o usuário. Identicador do usuário: {}, data alteracao: {}, cliente: {}, resultado: {}",login,dataAlteracao,clienteId,resource);
+        LOGGER.debug("GET USUARIO | Concluído | Obtem o usuário. Identicador do usuário: {}, cliente: {}, data alteracao: {}, resultado: {}",login,clienteId,dataAlteracao,resource);
         return new ResponseEntity<>(resource,OK);
     }
 
--- rest-api/src/main/java/br/com/silvanopessoa/rest/api/controller/doc/Doc.java
+++ rest-api/src/main/java/br/com/silvanopessoa/rest/api/controller/doc/Doc.java
@@ -27,12 +27,24 @@ public final class Doc {
 	/** A Constante READ_SCOPE. */
 	public static final String READ_SCOPE = "read";
 	
+	/** A Constante WRITE_SCOPE. */
+	public static final String WRITE_SCOPE = "write";
+	
 	/** A Constante READ_DESCRIPTION. */
 	public static final String READ_DESCRIPTION = "read only";
+	
+	/** A Constante WRITE_DESCRIPTION. */
+	public static final String WRITE_DESCRIPTION = "write only";
 	
 	/** A Constante USUARIO_NOTES_GET_BY_LOGIN. */
 	public static final String USUARIO_NOTES_GET_BY_LOGIN ="<br><b>Descrição:</b> Obtem usuario pelo identificador informado.<br><br> <b>Clientes:</b> Todos.<br><b>Scope:</b> read<br><b>Roles System:</b> ROLE_CLIENT<br><b>Roles User:</b> ROLE_REST_USER_FIND<br><b>Tarefas:</b> <a href=\"https://github.com/silvanopessoa/rest\" target=\"_blank\">#1</a><br><b>Documentação Canônico:</b> Não informado.<br><b>Documentação Teste:</b> Não informado.<br><b>Tipos de Autenticação:</b> password<br>";
 
 	/** A Constante USUARIO_VALUE_GET_BY_LOGIN. */
 	public static final String USUARIO_VALUE_GET_BY_LOGIN = "Obtem um usuário pelo seu identificador.";
+	
+	/** A Constante USUARIO_VALUE_DELETE_BY_LOGIN. */
+	public static final String USUARIO_VALUE_DELETE_BY_LOGIN = "Inativa um usuário pelo seu identificador.";
+	
+	/** A Constante USUARIO_NOTES_DELETE_BY_LOGIN. */
+	public static final String USUARIO_NOTES_DELETE_BY_LOGIN = "";
 }
